ac7c51af50ae9e69527f373f329d9243279ce80c
Evelina Ioannou <evelinaioannou2@gmail.com>
2020-12-14 10:37:57 +0200

    refactor: Convert to SCSS, import styles index.js

--- src/client/index.js
+++ src/client/index.js
@@ -1,1 +1,21 @@
 // Entry point for Webpack build tool
+
+import {getWeather} from './js/app.js';
+import {postData} from './js/app.js';
+
+import menu from './media/menu.svg';
+import search from './media/search.svg';
+
+import './styles/_colors-typo.scss'
+import './styles/resets.scss';
+import './styles/form.scss';
+import './styles/media-queries.scss';
+import './styles/base.scss'
+
+alert('I EXIST');
+
+
+export {
+    getWeather,
+    postData
+}
