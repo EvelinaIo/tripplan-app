434c991b7917b97343a62ceca9de5e9cb0e2b6d8
Evelina Ioannou <evelinaioannou2@gmail.com>
2020-12-20 12:58:20 +0200

    feat: Add day countdown function

---
+++ src/client/js/dateCount.js
@@ -0,0 +1,29 @@
+// Create a new date instance dynamically with JS
+let nowDate = new Date();
+// Set UTC hours to 00:00:00:00 to make sure we get an even number of days each time
+// endtime by default returns UTC 00:00:00:00
+nowDate.setUTCHours(0,0,0,0);
+// Convert to ISO as the date we receive from date picker
+console.log(nowDate.toISOString());
+
+export function getTimeRemaining(endtime){
+    // Create a variable total, to hold the remaining time until the deadline
+    //The Date.parse() function converts a time string into a value in milliseconds
+    //This allows us to subtract two times from each other and get the amount of time in between
+    const endDate = new Date(endtime).toISOString();
+    console.log(endDate);
+    const total = Date.parse(endDate) - Date.parse(nowDate);
+    console.log(total);
+    // Convert the milliseconds to days
+    const days = Math.floor( total/(1000*60*60*24));
+
+    if (days<1){
+      alert("Sorry, no time travel available.")
+      console.log("Date inserted is in the past")
+    } else {
+      console.log(days);
+      return days;
+    }
+  }
+
+
