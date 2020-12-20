29d8f436dbb6542fa4acb4539162771f125c6837
Evelina Ioannou <evelinaioannou2@gmail.com>
2020-12-20 20:44:02 +0200

    feat: Call server with allData as param
    All api cals in one js through a server call. Initiate weatherbit call.

---
+++ src/client/js/apiCalls.js
@@ -0,0 +1,36 @@
+const fetch = require('node-fetch')
+import {extractCityData} from './extractions.js'
+import {extractWeatherData} from './extractions.js'
+
+export async function callApis (allData) {
+    //Call Geonames Api
+    const geoData = await callServer('http://localhost:8081/geo', allData)
+    allData['cityData'] = extractCityData(geoData);
+    console.log(allData.cityData);
+
+    const weatherData = await callServer('http://localhost:8081/weather', allData)
+    allData['weatherData'] = extractWeatherData(weatherData);
+
+    return allData;
+}
+
+// Async function to post url to our server and retrieve external api data
+export async function callServer(url, allData) {
+    console.log(url);
+    console.log(allData.cityData);
+    try {
+        const response = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/JSON',
+            },
+        // Body data type must match "Content-Type" header        
+        body: JSON.stringify(allData)
+        });
+        const responseJSON = await response.json();
+        return responseJSON;
+        } catch(error) {
+            console.log(error)
+        }
+}
