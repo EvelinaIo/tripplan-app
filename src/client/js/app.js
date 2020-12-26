164d7fd7b9fb6558cf5d6fb1fb41440d5594310b
Evelina Ioannou <evelinaioannou2@gmail.com>
2020-12-26 11:50:27 +0200

    fix: Condition for Safari input date
    Safari returns date in dd/mm/yyyy which messes up day calculations

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,6 +1,14 @@
+/* This is the starting point of our functionality
+ * An event listener is added to the button with generate id
+ * After click all userInput is received to be stored in allData object
+ * For dates inserted, if format received is dd-mm-yyyy it is first converted to yyyy-mm-dd
+ * to hand over to getTimeRemaining in dateCount.js for the appropriate calculations to occur
+ * Then allData is used as a parameter for all apiCalls
+*/
+
 import {getTimeRemaining} from './dateCount.js';
 import {callApis} from './apiCalls.js';
-import {formatDate} from './extractions.js';
+import {displayDate} from './extractions.js';
 import {updateUI} from './resultsUI.js'
 
 /* Global Variables */
@@ -29,7 +37,7 @@ export async function performAction(event) {
     } 
 
     // Get departure date
-    const newDepart = document.getElementById('departure').value;
+    let newDepart = document.getElementById('departure').value;
     console.log(`Departure: ${newDepart}`);
     if(newDepart == '') {
         errorMsg.style.display = 'block';
@@ -37,9 +45,11 @@ export async function performAction(event) {
         document.getElementById('departure').required = true;
         return
     }
+    // Convert date to yyyy-mm-dd for calculations, if user is in Safari - see below
+    newDepart = convertDate(newDepart);
 
     // Get return date
-    const newReturn = document.getElementById('return').value;
+    let newReturn = document.getElementById('return').value;
     console.log(`Return: ${newReturn}`);
     if(newReturn == '') {
         errorMsg.style.display = 'block';
@@ -47,14 +57,22 @@ export async function performAction(event) {
         document.getElementById('return').required = true;
         return
     }
+    // Convert date to yyyy-mm-dd for calculations, if user is in Safari - see below
+    newReturn = convertDate(newReturn);
 
+    /* Initiate calculations*/
+    // New date instance
+    let nowDate = new Date();
+    // Set UTC hours to 00:00:00:00 to make sure we get an even number of days each time
+    // endtime by default returns UTC 00:00:00:00
+    nowDate.setUTCHours(0,0,0,0);
     // Calculate days untile departure, return and trip duration
-    const daysUntilDepart = getTimeRemaining(newDepart);
-    const daysUntilReturn = getTimeRemaining(newReturn);
+    const daysUntilDepart = getTimeRemaining(newDepart, nowDate);
+    const daysUntilReturn = getTimeRemaining(newReturn, nowDate);
     const tripDuration = daysUntilReturn - daysUntilDepart;
     console.log(tripDuration);
 
-    // Make sure return date is not sooner that departure
+    // Check if return date is not sooner that departure
     if (tripDuration<0) {
         errorMsg.style.display = 'block';
         errorMsg.innerHTML = 'Your Return is sooner that your Depart. Please insert valid dates.';
@@ -62,6 +80,7 @@ export async function performAction(event) {
         return
     }
 
+    // Check if dates are in the past - getTimeRemaining returns null for negative amount of days
     if (daysUntilDepart == null || daysUntilReturn == null) {
         errorMsg.style.display = 'block';
         errorMsg.innerHTML = 'Sorry, no time travel available. Please insert valid dates.';
@@ -72,8 +91,8 @@ export async function performAction(event) {
     // Initiate allData object to store all user input
     let allData = {};
     // Store newDepart and newReturn 
-    allData["userInput"]= { newLocation , newDepart: formatDate(newDepart), newReturn: formatDate(newReturn), daysUntilDepart, daysUntilReturn, tripDuration };
-    
+    allData["userInput"]= { newLocation , newDepart: displayDate(newDepart), newReturn: displayDate(newReturn), daysUntilDepart, daysUntilReturn, tripDuration };
+
     // Run callApis function to retrieve all data from api calls, then updateUI using that data
     allData = await callApis(allData);
 
@@ -81,3 +100,24 @@ export async function performAction(event) {
     updateUI(allData);
 }
 
+/* Here we convert the date input format.
+ * From Chrome we receive a format of yyyy-mm-dd and from Safari we receive dd-mm-yyyy.
+ * To make sure that we calculate the days properly we need to format the date to yyyy-mm-dd.
+*/
+
+// This regex describes the dd-mm-yyyy format
+const date_regex = /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/i;
+// We check if the input date matched the regex and proceed to conversion
+function convertDate(dateString){
+    if (!(date_regex.test(dateString))) {
+      const okDate = dateString;
+      console.log('It is OK');
+      return okDate;
+    } else {
+      const thisDate = dateString.split('/');
+      console.log(thisDate);
+      const newDate = [thisDate[2],thisDate[1],thisDate[0] ].join("-");
+
+      return newDate;
+    }         
+  }
